829d56541cb81a848e22b33bc73026c5653b03a0
Noldor5150 <ruikis.paulius@gmail.com>
2019-04-24 18:27:45 +0300

    paisom piramides su for ciklais

--- script.js
+++ script.js
@@ -51,27 +51,27 @@
 // });
 
 
-function sum(firstNumber, secondNumber){
-  return firstNumber + secondNumber;
-}
-function minus(firstNumber, secondNumber){
-  return firstNumber  - secondNumber;
-}
-function division(firstNumber, secondNumber){
-  return firstNumber / secondNumber;
-}
-function multiply(firstNumber, secondNumber){
-  return firstNumber * secondNumber;
-}
-function modulus(firstNumber, secondNumber){
-  return firstNumber % secondNumber;
-}
+// function sum(firstNumber, secondNumber){
+//   return firstNumber + secondNumber;
+// }
+// function minus(firstNumber, secondNumber){
+//   return firstNumber  - secondNumber;
+// }
+// function division(firstNumber, secondNumber){
+//   return firstNumber / secondNumber;
+// }
+// function multiply(firstNumber, secondNumber){
+//   return firstNumber * secondNumber;
+// }
+// function modulus(firstNumber, secondNumber){
+//   return firstNumber % secondNumber;
+// }
 
 
 
-let result = document.getElementById("result");
-let firstNumber = ()=> Number(document.getElementById("firstnumber").value);
-let secondNumber = ()=> Number(document.getElementById("secondnumber").value);
+// let result = document.getElementById("result");
+// let firstNumber = ()=> Number(document.getElementById("firstnumber").value);
+// let secondNumber = ()=> Number(document.getElementById("secondnumber").value);
 
 // let btnPlius = document.getElementById("plius");
 //     btnPlius.addEventListener("click", function(event){
@@ -101,35 +101,107 @@ let secondNumber = ()=> Number(document.getElementById("secondnumber").value);
 //       document.getElementById("result").textContent = modulus (firstNumber() , secondNumber());
 //     });  
    
-let myDiv = document.getElementById("myDiv");
+// let myDiv = document.getElementById("myDiv");
 
-myDiv.addEventListener("click", function(event){
-  console.log(event);
-  let elementId = event.target.id;
-  switch (elementId) {
-    case "plius":
-    document.getElementById("result").textContent =sum(firstNumber(),secondNumber());
-      break;
-    case "minus":
-    document.getElementById("result").textContent =minus(firstNumber(),secondNumber());
-      break;   
-    case "multiply":
-    document.getElementById("result").textContent =multiply(firstNumber(),secondNumber());
-      break;
-    case "division":
-    document.getElementById("result").textContent =division(firstNumber(),secondNumber());
-      break;
-    case "mod":
-    document.getElementById("result").textContent =modulus(firstNumber(),secondNumber());
-      break; 
-    case "clear":
-    document.getElementById("result").textContent = "";
-    document.getElementById("firstnumber").value = "";
-    document.getElementById("secondnumber").value = "";
-      break;      
+// myDiv.addEventListener("click", function(event){
+//   console.log(event);
+//   let elementId = event.target.id;
+//   switch (elementId) {
+//     case "plius":
+//     document.getElementById("result").textContent =sum(firstNumber(),secondNumber());
+//       break;
+//     case "minus":
+//     document.getElementById("result").textContent =minus(firstNumber(),secondNumber());
+//       break;   
+//     case "multiply":
+//     document.getElementById("result").textContent =multiply(firstNumber(),secondNumber());
+//       break;
+//     case "division":
+//     document.getElementById("result").textContent =division(firstNumber(),secondNumber());
+//       break;
+//     case "mod":
+//     document.getElementById("result").textContent =modulus(firstNumber(),secondNumber());
+//       break; 
+//     case "clear":
+//     document.getElementById("result").textContent = "";
+//     document.getElementById("firstnumber").value = "";
+//     document.getElementById("secondnumber").value = "";
+//       break;      
       
-    default:
-      break;
-  }
-});
+//     default:
+//       break;
+//   }
+// });
 
+// function pyramid(){
+// for(i = 0; i < 7; i++){
+//   for(y = 0; y < i; y++)
+//     {
+//       document.getElementById("result").innerHTML = "*";
+//     };
+  
+//     document.getElementById("result").innerHTML = "a";
+//   }
+// };
+
+function triangle(num) {
+  var output = "";
+  for (var i = 1; i <= num; i++) {
+    for (var j = 1; j <= i; j++) {
+      output += j + " ";
+    }
+    output += "<br/>";
+  }
+  document.getElementById("result").innerHTML = output;
+};
+
+let btnTriangle = document.getElementById("triangle");
+    btnTriangle.addEventListener("click", function(event){
+    triangle (10);
+    });
+
+function pyramid(n) {
+    var src = "";
+    var space = "&nbsp;";
+
+  for (var i = 0; i < n; i++) {
+    for (var j = 1; j < n-i; j++) {
+      src += space + space;
+    }
+    for (var k = 1; k <= (2*i+1); k++) {
+      src += "*" ;
+    }
+    src += "<br/>";
+  }
+  document.getElementById("result").innerHTML = src;
+}
+
+let btnPyramid = document.getElementById("pyramid");
+    btnPyramid.addEventListener("click", function(event){
+      pyramid (5);
+    });
+
+    
+function reversePyr(n) {
+    var src = "";
+    var space = "&nbsp;";
+    var z = n + 1;
+  for (var i = 0; i < z; i++) {
+    src += space;
+    
+    for (var k = 1; k <= (2*i+1); k++) {
+      src += space;
+    }
+    for (var j = 1; j < z-i; j++) {
+     
+      src += space + j + space;
+    }
+    src += "<br/>";
+  }
+  document.getElementById("result").innerHTML = src;
+}
+
+let btnRevPyramid = document.getElementById("reversePyr");
+    btnRevPyramid.addEventListener("click", function(event){
+      reversePyr (6);
+    });    
